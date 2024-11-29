08a5ba3b9131b9d9472f44568f99916bc2683129
Ryszard <ryszardpiekarski@outlook.com>
2024-11-29 10:20:49 +0100

    A new feature2 on feature-2 branch

--- src/main/java/eu/chrost/git/Feature.java
+++ src/main/java/eu/chrost/git/Feature.java
@@ -3,5 +3,6 @@ package eu.chrost.git;
 public class Feature {
     public static void main(String[] args) {
         System.out.println("My new super feature");
+        System.out.println("My new super feature-2");
     }
 }
