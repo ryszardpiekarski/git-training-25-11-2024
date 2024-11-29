db6c39d79690612b87a98ff24cd47071f235f161
Ryszard <100772312+ryszardpiekarski@users.noreply.github.com>
2024-11-29 09:40:38 +0100

    Update Main.java

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,8 +1,14 @@
 package eu.chrost.git;
 
+import java.util.Random; 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+
+        Random rand = new Random();
+        int value = rand.nextInt(100);
+        System.out.println(value);
     }
 }
