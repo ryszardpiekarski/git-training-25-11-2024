6b718023e157ed5e5c8f649d33183d81b1f6c641
Ryszard <ryszardpiekarski@outlook.com>
2024-11-29 09:40:27 +0100

    Added changes

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,8 +1,13 @@
 package eu.chrost.git;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
     }
 }
